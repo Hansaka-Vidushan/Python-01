df7baf7d88b88dec30bbc6bcea5ba3fe92d208e4
Hansaka-Vidushan <hansakavidushan56@gmail.com>
2025-02-26 14:33:46 +0530

    This is explained to working Packed arguments & Keyword arguments & Return.

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("Hansaka")
+#Packed Arguments
+def subjects(*marks):
+    total = 0
+    for i in marks:
+        total += i
+    print(total)
+
+subjects(10,10,10)
+
+#keyword Argument
+def g_form(**keyword):
+    if 'name' not in keyword:
+        print("Error")
+    else:
+        print("Hello" , keyword['name'])
+
+    if 'age' not in keyword:
+        print("Error")
+    else:
+        print("You are", keyword['age'] , "years old")
+
+    if 'city' not in keyword:
+        print("Error")
+    else:
+        print("You're from" , keyword['city'])
+
+g_form(name = "Hansaka" , age = 18 , city = "Maradankadawala")
+g_form(age = 19 , city="kekirawa")
+
+#Return
+def grade(marks):
+
+    if marks < 0 or marks > 100:
+        print ("Invalid")
+    elif marks < 35:
+        get_grade = "W"
+    elif marks < 55:
+        get_grade = "C"
+    elif marks < 65:
+        get_grade =  "B"
+    else:
+        get_grade = "A"
+
+    print("Hansaka Vidushan")
+    return get_grade
+
+get_grade = grade(55)
+print(get_grade)
